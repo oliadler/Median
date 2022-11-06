23885a0f7a729b8f8771abd0518b8cf4e37b522f
oliadler <oliver_elias.adler@fh-bielefeld.de>
2022-11-06 10:27:45 +0100

    Kommentare hinzugefügt

--- src/Median.java
+++ src/Median.java
@@ -1,19 +1,28 @@
-import java.sql.Array;
 
 public class Median {
 
     public int median(int[] array){
 
+        // Überprüfung, ob das array eventuell leer ist.
+
+        if (array == null || array.length == 0)
+        {
+            System.out.println("Das Array ist leer.");
+        }
+
+        // array wird sortiert.
         java.util.Arrays.sort(array);
+        // array Länge wird in eine Variable geschrieben.
         double arrayLength = array.length;
         double medianIndex;
 
         if (array.length % 2 != 0)
         {
+            // Formel zur Berechnung des Medians bei einer ungeraden Anzahl von Elementen im Array
             return (array.length +1 )/2;
         }
         else
-        {
+        {   // Formel zur Berechnung des Medians bei einer geraden Anzahl von Elementen im Array
             medianIndex =((( arrayLength/2)+(arrayLength/2)+1))/2;
         }
 
--- src/MedianTest.java
+++ src/MedianTest.java
@@ -2,16 +2,19 @@ public class MedianTest {
 
     public static void main(String[] args) {
 
+        // Ein neues Objekt der Klasse Median wird erstellt.
         Median medianClass = new Median();
 
+        // Testdurchlauf mit array1
         int[] array1 = {12,16,2,63,55,8};
         int medianIndexArray1 = medianClass.median(array1);
 
-        System.out.println("Der Median lautet: "+array1[medianIndexArray1]);
+        System.out.println("Der Median für Array1 lautet: "+array1[medianIndexArray1]);
 
+        // Testdurchlauf mit array2
         int [] array2 = {42,7,543,77,87,876,143};
         int medianIndexArray2 = medianClass.median(array2);
-        System.out.println("Der Median lautet: "+array2[medianIndexArray2]);
+        System.out.println("Der Median für Array2 lautet: "+array2[medianIndexArray2]);
 
 
     }
